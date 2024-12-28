fe30b763c069d91fd22d8e402d062dc52ecc97f6
soklakak <soklakak493@gmail.com>
2024-12-28 23:21:20 +0700

    Add files via upload

---
+++ a.js
@@ -0,0 +1,5 @@
+// Function to reveal the birthday surprise message
+function showMessage() {
+    const surpriseMessage = document.getElementById('surpriseMessage');
+    surpriseMessage.classList.remove('hidden');
+  }
